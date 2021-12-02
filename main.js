d7ff250684f45b898a3e8c20b30d8b110d3455a8
Alender Guerrero <82161317+Alender17@users.noreply.github.com>
2021-12-01 23:18:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let caja = document.getElementById('subir_arriba');
+caja.addEventListener("click", function(){
+    document.documentElement.scrollTop = 0 
+})
+
+window.addEventListener("scroll", function(){
+    if(document.documentElement.scrollTop >0){
+        caja.style.display = "flex";
+    }
+    else{
+        caja.style.display = "none";
+    }
+})
